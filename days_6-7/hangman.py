fbda10a1de3c0272b37ea9de891f84a823b632b8
Karim Ahmadi <karim.ahmadi@tuni.fi>
2025-08-14 14:23:33 +0300

    added day 6-7 projects coverin while loop, function, random module in python

---
+++ days_6-7/hangman.py
@@ -0,0 +1,61 @@
+import random
+import hangman_words
+import hangman_art
+
+
+lives = 6
+
+print(hangman_art.logo)
+chosen_word = random.choice(hangman_words.word_list)
+#print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+
+
+    print(f"**************************** {lives} LIVES LEFT ****************************")
+    guess = input("Guess a letter: ").lower()
+
+
+    if guess in correct_letters:
+        print(f"You guessed already the letter: {guess}")
+        continue
+    else:
+        correct_letters.append(guess)
+
+
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        print(f"You guessed the letter: {guess}, that's not in the word. you lose a life")
+        lives -= 1
+
+        if lives == 0:
+            game_over = True
+
+            print(f"***********************YOU LOSE********************** \n THE CORRECT WORD IS { chosen_word}")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+    print(hangman_art.stages[lives])
---
+++ days_6-7/hangman_words.py
@@ -0,0 +1,10 @@
+word_list = [
+    "keinulauta", "kovakuoriainen", "pehmolelu", "lintulauta", "kruunu",
+    "nokkahuilu", "suudelma", "häntä", "nurmikko", "pakoputki",
+    "jalanjälki", "juustohöylä", "hirsimökki", "ratti", "vesiränni",
+    "takaraivo", "solmio", "sateenvarjo", "viemäri", "silmäluomi",
+    "piilolinssit", "sisilisko", "lasikuula", "monot", "koukku",
+    "salpa", "miljoonakala", "nuottiavain", "kenkälusikka", "kaiku",
+    "kitalaki", "lommo", "ankkuri", "harppi", "pulpetti",
+    "avaimenperä", "ankerias", "kummitus", "airot", "hikipisara"
+]
